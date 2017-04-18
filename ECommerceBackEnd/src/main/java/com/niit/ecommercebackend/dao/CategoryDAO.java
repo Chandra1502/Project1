dfb779146e3fce7c97784f500b93f13812333299
Chandra1502 <chandu.pcv@gmail.com>
2017-04-18 17:37:36 +0530

    Data Access object created for Category.

--- ECommerceBackEnd/src/main/java/com/niit/ecommercebackend/dao/CategoryDAO.java
+++ ECommerceBackEnd/src/main/java/com/niit/ecommercebackend/dao/CategoryDAO.java
@@ -1,5 +1,7 @@
 package com.niit.ecommercebackend.dao;
 
+import java.util.List;
+
 import com.niit.ecommercebackend.model.Category;
 
 public interface CategoryDAO {
@@ -7,5 +9,9 @@ public interface CategoryDAO {
 	public boolean saveOrUpdate(Category category);
 	
 	public boolean delete(Category category);
+	
+	public Category get(int id);
+	
+	public List<Category> list();
 	
 }
--- ECommerceBackEnd/src/main/java/com/niit/ecommercebackend/dao/CategoryDAOImpl.java
+++ ECommerceBackEnd/src/main/java/com/niit/ecommercebackend/dao/CategoryDAOImpl.java
@@ -1,13 +1,14 @@
 package com.niit.ecommercebackend.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.niit.ecommercebackend.model.Category;
 
 @Repository(value="categoryDAO")
@@ -50,6 +51,41 @@ public class CategoryDAOImpl implements CategoryDAO {
 			e.printStackTrace();
 			return false;
 		}
+	}
+
+	@Override
+	public Category get(int id) {
+		String hql = "from Category where category_id= " + id;
+		Session s = sessionFactory.getCurrentSession();
+		Transaction t = s.beginTransaction();
+		Query query = s.createQuery(hql);
+		List<Category> list = query.list();
+		t.commit();
+		if(list == null || list.isEmpty())
+		{
+			System.out.println("No products are available with this id: "+id);
+			return null;
+		}
+		else
+			return list.get(0);
+	}
+
+	@Override
+	public List<Category> list() {
+		Session s = sessionFactory.getCurrentSession();
+		Transaction t = s.beginTransaction();
+		String hql = "from Category";
+		Query query = s.createQuery(hql);
+		
+		System.out.println("Starting of the method List");
+		List<Category> list = query.list();
+		
+		if(list==null||list.isEmpty())
+		{
+			System.out.println("No categories are available");
+		}
+		t.commit();
+		return query.list();
 	}
 
 }
