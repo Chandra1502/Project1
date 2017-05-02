9c7141351a41a5282f109c8c98a5fb4326e6f127
Chandra1502 <chandu.pcv@gmail.com>
2017-05-02 21:39:00 +0530

    Spring Security added by making changes to web.xml, Home Controller, Login page and by adding the spring-security.xml file.

--- ECommerceFrontEnd/src/main/java/com/niit/ecommercefrontend/controller/HomeController.java
+++ ECommerceFrontEnd/src/main/java/com/niit/ecommercefrontend/controller/HomeController.java
@@ -1,17 +1,37 @@
 package com.niit.ecommercefrontend.controller;
 
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.niit.ecommercebackend.dao.UserDAO;
+import com.niit.ecommercebackend.model.User;
 
 
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	UserDAO userDAO;
 	
+	@Autowired
+	User user;
+	
+	@Autowired
+	SessionFactory sessionFactory;
 	
 	@RequestMapping("/")
 	public String showHome()
@@ -51,12 +71,51 @@ public class HomeController {
 			mv=new ModelAndView("Login");
 			return mv;
 		}
+	}	
+	
+	@RequestMapping(value = "/login_session_attributes")
+	public String login_session_attributes(HttpSession session, Model model, @RequestParam(value="username") String id){
+		
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+		
+		System.out.println("inside security check");
+		
+		session.setAttribute("name", name);
+		
+		user = userDAO.get(id);
+		int x = user.getUserid();
+		session.setAttribute("email", user.getEmailid());
+		session.setAttribute("LoggedInUser", user.getUsername());
+		session.setAttribute("LoggedInUserID", x);
+		session.setAttribute("LoggedIn", "true");
+		
+		@SuppressWarnings("unchecked")
+		Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		
+		String role = "ROLE_USER";
+		for(GrantedAuthority authority : authorities){
+			if(authority.getAuthority().equals(role)){
+				return "Home"; //It has to be View Products page. Since the page is not created we are redirecting to it home.
+			}
+			else{
+				session.setAttribute("isAdmin", "true");
+			}
+		}
+		return "AdminHome";
 	}
 	
-	
-	
-	
+	@RequestMapping("/perform_logout")
+	public ModelAndView showLogout(HttpServletRequest request, HttpSession session){
+		System.out.println("logout");
+		ModelAndView mv = new ModelAndView("Home");
+		session.invalidate();
+		session = request.getSession(true);
+		mv.addObject("LogOutMessage","You have Successfully Logged Out.");
+		mv.addObject("loggedOut","true");
+		return mv;
+	}
 }
+
 
  
 
--- ECommerceFrontEnd/src/main/webapp/WEB-INF/views/Login.jsp
+++ ECommerceFrontEnd/src/main/webapp/WEB-INF/views/Login.jsp
@@ -1,52 +1,15 @@
 <%@include file="Header.jsp"%>
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-6 col-xs-12">
-			<form action="Validate">
-				<h4 align="left" style="color: midnightblue">Existing User,Login here:</h4>
-				<table style="float:left">
-					<tr>
-						<th>Username</th>
-						<td align="left"><input type="text" name="username"></td>
-					</tr>
-					<tr>
-						<th>Password</th>
-						<td align="left"><input type="password" name="password"></td>
-					</tr>
-					<tr>
-						<td></td>
-						<td align="right"><input type="submit" name="submitlogin"
-							value="Ok"></td>
-				</table>
-			</form>
-		</div>
-		<div class="col-md-6 col-xs-12">
-			<h4 style="color: midnightblue; float: right;">New User, Register here:</h4>
-			<table style="float:right">
-				<tr>
-					<th style="float: left">Username</th>
-					<td><input type="text" name="user"></td>
-				</tr>
-				<tr>
-					<th style="float: left">Password</th>
-					<td><input type="password" name="pass"></td>
-				</tr>
-				<tr>
-					<th style="float: left">Confirm Password</th>
-					<td><input type="password" name="confirmpassword"></td>
-				</tr>
-				<tr>
-					<th style="float: left">E-mail</th>
-					<td><input type="text" name="email"></td>
-				</tr>
-				<tr>
-					<th></th>
-					<td style="float: right"><input type="submit"
-						name="submitregister"></td>
-			</table>
 
-		</div>
-	</div>
-</div>
+
+<form action="perform_login" method="post">
+<div class="login" style="position:auto">
+		<h2>Login Form</h2>
+		<input type="text" name="username" placeholder="Enter your username"><br>
+		<input type="password" name="password" placeholder="Enter your password"><br>
+		<button class="btn-login">Login</button>
+</div>	
+</form>
+
+
 <%@include file="Footer.jsp"%>
